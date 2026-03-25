a2fb7aebc7183e2cf585861977910448b3cc1b7d
MajestyCorp <mail.mikecs@gmail.com>
2026-03-25 20:19:45 +0100

    fix

--- Assets/Scripts/Model/SkillAttributes.cs
+++ Assets/Scripts/Model/SkillAttributes.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace DnD.Model
 {
@@ -13,48 +14,53 @@ namespace DnD.Model
         {
             attributes = new Dictionary<string, SkillAttribute>
             {
-                { "Акробатика", new SkillAttribute() },
-                { "Анализ", new SkillAttribute() },
-                { "Атлетика", new SkillAttribute() },
-                { "Внимательность", new SkillAttribute() },
-                { "Выживание", new SkillAttribute() },
-                { "Выступление", new SkillAttribute() },
-                { "Запугивание", new SkillAttribute() },
-                { "История", new SkillAttribute() },
-                { "Ловкость рук", new SkillAttribute() },
-                { "Магия", new SkillAttribute() },
-                { "Медицина", new SkillAttribute() },
-                { "Обман", new SkillAttribute() },
-                { "Природа", new SkillAttribute() },
-                { "Проницательность", new SkillAttribute() },
-                { "Религия", new SkillAttribute() },
-                { "Скрытность", new SkillAttribute() },
-                { "Убеждение", new SkillAttribute() },
-                { "Уход за животными", new SkillAttribute() },
+                { Normalize("Акробатика"), new SkillAttribute() },
+                { Normalize("Анализ"), new SkillAttribute() },
+                { Normalize("Атлетика"), new SkillAttribute() },
+                { Normalize("Внимательность"), new SkillAttribute() },
+                { Normalize("Выживание"), new SkillAttribute() },
+                { Normalize("Выступление"), new SkillAttribute() },
+                { Normalize("Запугивание"), new SkillAttribute() },
+                { Normalize("История"), new SkillAttribute() },
+                { Normalize("Ловкость рук"), new SkillAttribute() },
+                { Normalize("Магия"), new SkillAttribute() },
+                { Normalize("Медицина"), new SkillAttribute() },
+                { Normalize("Обман"), new SkillAttribute() },
+                { Normalize("Природа"), new SkillAttribute() },
+                { Normalize("Проницательность"), new SkillAttribute() },
+                { Normalize("Религия"), new SkillAttribute() },
+                { Normalize("Скрытность"), new SkillAttribute() },
+                { Normalize("Убеждение"), new SkillAttribute() },
+                { Normalize("Уход за животными"), new SkillAttribute() },
             };
+        }
+
+        private string Normalize(string s)
+        {
+            return s.Normalize(NormalizationForm.FormC);
         }
 
         public void Invalidate(CharacterData data)
         {
             var throws = data.throws;
-            attributes["Акробатика"].Invalidate(throws.dexterity);
-            attributes["Анализ"].Invalidate(throws.intelligence);
-            attributes["Атлетика"].Invalidate(throws.strength);
-            attributes["Внимательность"].Invalidate(throws.wisdom);
-            attributes["Выживание"].Invalidate(throws.wisdom);
-            attributes["Выступление"].Invalidate(throws.charisma);
-            attributes["Запугивание"].Invalidate(throws.charisma);
-            attributes["История"].Invalidate(throws.intelligence);
-            attributes["Ловкость рук"].Invalidate(throws.dexterity);
-            attributes["Магия"].Invalidate(throws.intelligence);
-            attributes["Медицина"].Invalidate(throws.wisdom);
-            attributes["Обман"].Invalidate(throws.charisma);
-            attributes["Природа"].Invalidate(throws.intelligence);
-            attributes["Проницательность"].Invalidate(throws.wisdom);
-            attributes["Религия"].Invalidate(throws.intelligence);
-            attributes["Скрытность"].Invalidate(throws.dexterity);
-            attributes["Убеждение"].Invalidate(throws.charisma);
-            attributes["Уход за животными"].Invalidate(throws.wisdom);
+            attributes[Normalize("Акробатика")].Invalidate(throws.dexterity);
+            attributes[Normalize("Анализ")].Invalidate(throws.intelligence);
+            attributes[Normalize("Атлетика")].Invalidate(throws.strength);
+            attributes[Normalize("Внимательность")].Invalidate(throws.wisdom);
+            attributes[Normalize("Выживание")].Invalidate(throws.wisdom);
+            attributes[Normalize("Выступление")].Invalidate(throws.charisma);
+            attributes[Normalize("Запугивание")].Invalidate(throws.charisma);
+            attributes[Normalize("История")].Invalidate(throws.intelligence);
+            attributes[Normalize("Ловкость рук")].Invalidate(throws.dexterity);
+            attributes[Normalize("Магия")].Invalidate(throws.intelligence);
+            attributes[Normalize("Медицина")].Invalidate(throws.wisdom);
+            attributes[Normalize("Обман")].Invalidate(throws.charisma);
+            attributes[Normalize("Природа")].Invalidate(throws.intelligence);
+            attributes[Normalize("Проницательность")].Invalidate(throws.wisdom);
+            attributes[Normalize("Религия")].Invalidate(throws.intelligence);
+            attributes[Normalize("Скрытность")].Invalidate(throws.dexterity);
+            attributes[Normalize("Убеждение")].Invalidate(throws.charisma);
+            attributes[Normalize("Уход за животными")].Invalidate(throws.wisdom);
         }
     }
 }
